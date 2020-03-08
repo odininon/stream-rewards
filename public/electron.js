d2b13e2610e8a2da71fc1f7696db0f5f8588d5bb
Michael Stengel <mike.stengel@gmail.com>
2020-03-08 13:46:33 -0500

    Fix travis

--- public/electron.js
+++ public/electron.js
@@ -1,4 +1,11 @@
 const electron = require("electron");
+const log = require("electron-log");
+const { autoUpdater } = require("electron-updater");
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = "info";
+log.info("App starting...");
+
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
@@ -8,7 +15,11 @@ const isDev = require("electron-is-dev");
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 900, height: 680 });
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+    webPreferences: { nodeIntegration: true }
+  });
   mainWindow.loadURL(
     isDev
       ? "http://localhost:3000"
@@ -34,4 +45,41 @@ app.on("activate", () => {
   if (mainWindow === null) {
     createWindow();
   }
+});
+
+autoUpdater.on("checking-for-update", () => {
+  sendStatusToWindow("Checking for update...");
+});
+
+autoUpdater.on("update-available", info => {
+  sendStatusToWindow("Update available.");
+});
+
+autoUpdater.on("update-not-available", info => {
+  sendStatusToWindow("Update not available.");
+});
+
+autoUpdater.on("error", err => {
+  sendStatusToWindow("Error in auto-updater. " + err);
+});
+
+autoUpdater.on("download-progress", progressObj => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + " - Downloaded " + progressObj.percent + "%";
+  log_message =
+    log_message +
+    " (" +
+    progressObj.transferred +
+    "/" +
+    progressObj.total +
+    ")";
+  sendStatusToWindow(log_message);
+});
+
+autoUpdater.on("update-downloaded", info => {
+  sendStatusToWindow("Update downloaded");
+});
+
+app.on("ready", function() {
+  autoUpdater.checkForUpdatesAndNotify();
 });
